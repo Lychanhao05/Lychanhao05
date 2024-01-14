4a0025954cda7796bc78507cac0acabc5e536712
Lychanhao05 <155815620+Lychanhao05@users.noreply.github.com>
2024-01-14 22:30:23 +0700

    ...

--- Code/123.cpp
+++ Code/123.cpp
@@ -10,12 +10,11 @@ using namespace std;
 void TextColor (int color)
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE) , color);
-}
+}sfsdsfsdfsdfdf
 
 void gotoXY (int column, int line)
 {
-	COORD coord;
-	coord.X = column;
+	COORD coord;sdadasdas
 	coord.Y = line;
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
